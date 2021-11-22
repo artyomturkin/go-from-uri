3f3ea71ce29e64ee0009251435399e148cbce419
zharqynqa <zharqynqa@halykbank.kz>
2021-11-22 15:28:33 +0600

    add init cache builder and get select from the file

--- go.mod
+++ go.mod
@@ -6,11 +6,14 @@ require (
 	github.com/Shopify/sarama v1.30.0
 	github.com/ThreeDotsLabs/watermill v1.1.1
 	github.com/ThreeDotsLabs/watermill-kafka/v2 v2.2.1
-	github.com/artyomturkin/go-from-uri v0.0.0-20211110111005-24ecc9acde0e
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
 	github.com/qairjar/watermill-elastic-plugin v0.0.0-20211105104720-7e96b57c5831
-	github.com/qairjar/watermill-sql-plugin v0.0.0-20211116090602-5daebfa8622b
+	github.com/qairjar/watermill-sql-plugin v0.0.0-20211122042829-adf32ba4573b
 	github.com/stretchr/testify v1.7.0
 	go.opentelemetry.io/otel/internal/metric v0.25.0 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
 )
--- sql/client.go
+++ sql/client.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill/message"
+	"io/ioutil"
+
 	plugin "github.com/qairjar/watermill-sql-plugin"
 	"net/url"
 )
@@ -33,13 +35,32 @@ func Open(connection string) (*dbSql.DB, error) {
 	return db, err
 }
 
+func getSelect(connection string) (string, error) {
+	u, err := url.Parse(connection)
+	if err != nil {
+		return "", nil
+	}
+	var selectQuery string
+	if len(u.Query().Get("select-path")) > 0 {
+		file, err := ioutil.ReadFile(u.Query().Get("select-path"))
+		if err != nil {
+			return "", nil
+		}
+		selectQuery = string(file)
+	}
+	return selectQuery, nil
+}
 func NewSubscriber(connection string, logger watermill.LoggerAdapter) (message.Subscriber, error) {
 	db, err := Open(connection)
 	if err != nil {
 		return nil, err
 	}
-	u, err := url.Parse(connection)
-	sub := plugin.Subscriber{DB: db, SelectPath: u.Query().Get("sql-path")}
+	selectQuery, err := getSelect(connection)
+	sub := plugin.Subscriber{DB: db, SelectQuery:selectQuery}
+	_, err = sub.InitCache(connection)
+	if err != nil {
+		return nil, err
+	}
 	return sub.NewSubscriber(nil, logger)
 }
 
